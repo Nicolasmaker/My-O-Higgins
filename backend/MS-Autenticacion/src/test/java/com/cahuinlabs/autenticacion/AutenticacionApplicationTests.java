297953f163ef4cafeb06b9edc81033449d7240b4
Benjamin Aravena <benj.aravenam@duocuc.cl>
2026-05-07 00:29:54 -0400

    Integracion de nuevas dependencias y cambio de java 17 a 25

--- backend/MS-Autenticacion/src/test/java/com/cahuinlabs/autenticacion/AutenticacionApplicationTests.java
+++ backend/MS-Autenticacion/src/test/java/com/cahuinlabs/autenticacion/AutenticacionApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.cahuinlabs.autenticacion;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApplicationTests {
+class AutenticacionApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
