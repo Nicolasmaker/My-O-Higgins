204d830918678a5b4170597878a366f7484c724a
francisca <fr.guerreroo@duocuc.cl>
2026-06-30 00:34:27 -0400

    fix: se arregla codigo descontinuado

--- backend/MS-CalendarioEscolar/src/main/java/com/myohiggins/calendario/config/RestClientConfig.java
+++ backend/MS-CalendarioEscolar/src/main/java/com/myohiggins/calendario/config/RestClientConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.client.RestClient;
 public class RestClientConfig {
 
     // Lee la URL del microservicio de Gestion Academica desde application.properties
-    // Si no está definida, usa localhost:8083 por defecto (coordinar puerto con el equipo)
+    // Si no está definida, usa localhost:8087 por defecto
     @Value("${app.services.gestionacademica-url:http://localhost:8087}")
     private String gestionAcademicaUrl;
 
